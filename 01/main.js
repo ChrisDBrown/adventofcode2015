ec0f141a3d91cd8dc6539d66d832c3b2232f9f81
Chris Brown <chris@greenhilldigital.com>
2015-12-18 16:00:36 +0000

    Day 1 solution

---
+++ 01/main.js
@@ -0,0 +1,22 @@
+(function () {
+   'use strict';
+
+	const fs = require('fs');
+
+	fs.readFile('input.txt', 'utf8', function (err, data) {
+		if (err !== null) {
+			console.error('Unable to read file');
+		} else {
+			let floor = 0;
+			for (let i = 0; i < data.length; i++) {
+				if (data[i] == '(') {
+					floor++;
+				} else {
+					floor--;
+				}
+			}
+			console.log(floor);
+		}
+	});
+
+}());
