22a7ed41cbf902dbeb8a57789c291b29645fad8c
Chris Brown <chris@greenhilldigital.com>
2015-12-19 23:13:40 +0000

    Day 04 answer

---
+++ 04/main.js
@@ -0,0 +1,27 @@
+(function () {
+   'use strict';
+
+	const crypto = require('crypto');
+
+	const seed = 'yzbqklnj';
+	let answer = false;
+	let i = 0;
+
+	while (!answer) {
+		// generate the hash for the current value of i
+		let hash = crypto
+			.createHash("md5")
+			.update(seed + i.toString())
+			.digest("hex");
+
+		// if it starts with '00000' that's our answer, otherwise loop
+		if (hash.substring(0,5) == '00000') {
+			answer = hash;
+		} else {
+			i++;
+		}
+	}
+
+	console.log(i);
+
+}());
