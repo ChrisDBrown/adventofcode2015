4092818531e48613182490a4ac12b489810aa8d6
Chris Brown <chris@greenhilldigital.com>
2015-12-24 15:49:56 +0000

    Day 02 part 2 solution

--- 02/main.js
+++ 02/main.js
@@ -3,25 +3,33 @@
 
 	const fs = require('fs');
 
+	function sortNumbers(a,b) {
+		return a - b;
+	}
+
 	fs.readFile('02/input.txt', 'utf8', function (err, data) {
 		if (err) throw err;
 		let parcels = data.toString().split("\n");
-		let total = 0;
+		let totalPaper = 0;
+		let totalRibbon = 0;
 
 		// transform into array of the 3 different side sizes, sorted low -> high
 		parcels = parcels.map(function(parcel) {
-			let sides = parcel.split('x');
-			return [sides[0] * sides[1], sides[1] * sides[2], sides[0] * sides[2]].sort(function(a, b) {
-				return a - b;
-			});
+			let sides = parcel.split('x').sort(sortNumbers);
+			let dimensions = [sides[0] * sides[1], sides[1] * sides[2], sides[0] * sides[2]].sort(sortNumbers);
+			return dimensions.concat(sides);
 		});
 
 		for (let i = 0; i < parcels.length; i++) {
 			// the total surface area of the parcel, plus the smallest side
-			total += ((2*parcels[i][0]) + (2*parcels[i][1]) + (2*parcels[i][2]) + parcels[i][0]);
+			const paperArea = ((2*parcels[i][0]) + (2*parcels[i][1]) + (2*parcels[i][2]) + parcels[i][0]);
+			totalPaper += parseInt(paperArea, 10);
+			const ribbonLength = (2*parcels[i][3] + 2*parcels[i][4]) + (parcels[i][3] * parcels[i][4] * parcels[i][5]);
+			totalRibbon += parseInt(ribbonLength, 10);
 		}
 
-		console.log(total);
+		console.log('Part 1: ' + totalPaper);
+		console.log('Part 2: ' + totalRibbon);
 	});
 
 }());
